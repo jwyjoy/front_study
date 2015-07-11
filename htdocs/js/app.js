2fe2c63537a7487f80fe421adffbcff261c05529
jwyjoy <wkddn1235@gmail.com>
2015-07-11 21:03:04 +0900

    entity serviceに分けやすいように refactoring

--- htdocs/js/app.js
+++ htdocs/js/app.js
@@ -1,71 +1,109 @@
-angular.module('jangStudy',[])
-.controller('MainCtrl', function () {
-    var vm = this;
-    var count = 1;
-    var succeeded = ['failed', 'rarity', 'normal'];
-    vm.cookies = []; 
-    vm.money = 0;
-    vm.cooking_message = '追加!';
-    vm.alert_message = '';
-    vm.create_count = 1;
-    vm.item_name = 'item1(¥500)';
+angular.module('cookiesStorage', [])
+.value('cookies',[])
+.value('cookie_count', {value :1})
+.value('rarity', ['failed', 'rarity', 'normal']);
 
-    vm.cooking = function () {
-        for (var i = 0; i < vm.create_count; i++) {
-            if (vm.cookies.length < 10) {
+angular.module('money', [])
+.value('money', {value : 0})
+.value('sales',[50,500,100]);
 
-                var is_succeeded = getRandomInt(1,5) != 1;
-                console.log(is_succeeded);
-                if(!is_succeeded) {
-                     vm.cookies.push({
-                         id: count++,
-                         rarity: succeeded[0]
-                     });
-                }
-                else if(count %5 == 0){
-                     vm.cookies.push({
-                         id: count++,
-                         rarity: succeeded[1]
-                     });
-                } else {
-                     vm.cookies.push({
-                         id: count++,
-                         rarity: succeeded[2]
-                     });
-                }
+angular.module('item_list', [])
+.value('item_name', ['500円', '1000円', '完了'])
+.value('item_price', [500, 1000, 0])
+.value('item_rank', {value : 0});
 
-            } else {
-             vm.cooking_message = 'もう追加できませんぬ';
-            }
+angular.module('cookiesMaker', [
+    'cookiesStorage'
+]).factory('cookieMaker', function (cookies, cookie_count, rarity) {
+
+    return function () {
+        var is_succeeded = getRandomInt(1,5) != 1;
+        console.log(is_succeeded);
+
+        if(!is_succeeded) {
+             cookies.push({
+                 id: cookie_count.value++,
+                 rarity: rarity[0]
+             });
         }
-    };
-    vm.sellcooking = function (is_rarity, object_len) {
-        switch (is_rarity) {
-            case succeeded[0]:
-            vm.money+=50;
+        else if(cookie_count.value %5 == 0){
+             cookies.push({
+                 id: cookie_count.value++,
+                 rarity: rarity[1]
+             });
+        } else {
+             cookies.push({
+                 id: cookie_count.value++,
+                 rarity: rarity[2]
+             });
+        }
+    }
+});
+
+angular.module('cookiesSaler', [
+    'money','cookiesStorage'
+]).factory('cookie_saler', function (cookies, money, sales, rarity) {
+     return function (raritys, index) {
+        switch (raritys) {
+            case rarity[0]:
+            money.value += sales[0];
             break;
             
-            case succeeded[1]:
-            vm.money+=500;
+            case rarity[1]:
+            money.value += sales[1];
             break;
 
-            case succeeded[2]:
-            vm.money+=100;
+            case rarity[2]:
+            money.value += sales[2];
             break;
         }
-        vm.cookies.splice(object_len,1);
-    };
-    vm.item_double = function () {
-        if(vm.money < 500 * vm.create_count) {
-            vm.alert_message = '金が...ないんだ';      
-        } else {
-            vm.money -= 500 * vm.create_count ;
-            vm.create_count++;
-            vm.item_name = 'item2(¥1000)';
-        }
-    };
+        cookies.splice(index,1);
+    }
 });
 
 function getRandomInt(min, max) {
   return Math.floor( Math.random() * (max - min + 1) ) + min;
 }
+
+angular.module('jangStudy',['cookiesMaker', 'cookiesStorage', 'money', 'item_list', 'cookiesSaler'])
+.controller('MainCtrl', function (cookieMaker, cookies, cookie_count, rarity, money, sales, item_name, item_rank, item_price, cookie_saler) {
+    var vm = this;
+
+
+    vm.cooking_message = '追加!';
+    vm.alert_message = '';
+    vm.item_name = item_name[0];
+
+    vm.cookies = cookies;
+    vm.money = money.value;
+    vm.item_rank = item_rank.value;
+
+    vm.cooking = function () {
+        for (var i = 0; i < item_rank.value+1; i++) {
+
+            if (cookies.length < 10) {
+                cookieMaker();
+            } else {
+                vm.cooking_message = 'もう追加できませんぬ';
+            }
+        }
+    };
+
+    vm.sellcooking = function (raritys, index) {
+        cookie_saler(raritys, index);
+        vm.money = money.value;
+    };
+
+    vm.use_item = function () {
+        if(money.value < item_price[item_rank.value]) {
+            vm.alert_message = '金が...ないんだ';      
+        } else {
+            money.value -= item_price[item_rank.value];
+            item_rank.value++;
+            vm.item_name = item_name[item_rank.value];
+        }
+        vm.money = money.value;
+        vm.item_rank = item_rank.value;
+    };
+
+});
